abdad44bad7fcede9bae9f3be1770da4cc44abd9
sthefanyebianca <00001124252903SP@al.educacao.sp.gov.br>
2024-05-23 21:09:35 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background("lightgrey")
+
+  textFont("Comic Sans MS");
+  textSize(30)
+  textAlign(CENTER,CENTER);
+
+}
+
+function draw(){
+  text("Aprendendo JS na Alura",200,200)
+}
